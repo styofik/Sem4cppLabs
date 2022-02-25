8baf41bb17f21f6d7711c9e87b20fee71d785c72
styofik <styofik@gmail.com>
2022-02-25 03:05:34 +0300

    Third hw is ready

---
+++ algos.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Stefan Postnikov on 18.02.2022.
+//
+#include <iostream>
+
+#include <array>
+#include <vector>
+#include <deque>
+#include <list>
+#include <forward_list>
+#include <algorithm>
+
+#include <random>
+
+#include "timer.cpp"
+
+int main() {
+
+    return 0;
+}
